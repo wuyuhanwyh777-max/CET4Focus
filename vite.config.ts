ccc850bf8708a34885ac992709aab93a3345b31f
Yuhan Wu <wuyuhanwyh777@gmail.com>
2026-05-28 20:13:13 +0800

    修复 GitHub Pages base 路径

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/CET4Focus/",
   plugins: [react()],
   server: {
     port: 5173,
